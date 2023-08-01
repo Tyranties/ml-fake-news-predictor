95aff89508d008aff4c78fbbaaa439fde85aa333
TerenceCLZhang <terence.zhang2014@gmail.com>
2023-08-01 18:44:39 +1200

    removed main

--- backend/backend.py
+++ backend/backend.py
@@ -34,6 +34,3 @@ def predict():
         return jsonify({'Prediction': int(prediction[0])})
     except Exception as e:
         return jsonify({'error': str(e)})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
